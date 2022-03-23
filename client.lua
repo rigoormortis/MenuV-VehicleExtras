9f2aecf11bef63a0672b73164be248c571b4b6c4
rigoormortis <50382124+rigoormortis@users.noreply.github.com>
2022-03-23 19:08:07 -0400

    apparently it was needed

--- client.lua
+++ client.lua
@@ -40,6 +40,7 @@ end)
 
 menu2:On('open', function(m)
   m:ClearItems()
+  local veh = GetVehiclePedIsIn(PlayerPedId(), false)
   local availableExtras = {}
   local items = {}
   local extrasExist = false
